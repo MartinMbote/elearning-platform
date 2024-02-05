9b3672fc6b3118f3e92f13447d07bf8ac0b50f47
Martin <martinmmbote@gmail.com>
2024-02-05 19:01:19 +0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import logo from "./strathlogo.png";
+
+export { logo };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { logo } from '../assets';
+
+const Navbar = () => {
+  return (
+    <div>
+        <p className='text-red-500'>
+            Written
+        </p>
+    </div>
+  )
+}
+
+export default Navbar
