d6e3804f4ec063e42a6748c509493dfd6c09ffdb
Martin <martinmmbote@gmail.com>
2024-02-07 15:32:23 +0300

    fun section begun

--- src/components/FunSection.jsx
+++ src/components/FunSection.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 const FunSection = () => {
   return (
-    <div className='mt-[0.7vw] ml-[0.5vw] w-[99vw] h-[50vw] rounded-[0.7vw] bg-strathmore-blue'>
-        <div className='text-center'>
-            <h1 className='text-strathmore-yellow-light text-[2.2vw] font-bold'>
-                Fun, Engaging and Authentic
-            </h1>
+    <div>
+        <div className='mt-[0.7vw] ml-[0.5vw] w-[99vw] h-[82.3vw] rounded-[0.7vw] bg-strathmore-blue'>
+            <div className='flex'>
+                <div className='mt-[6.5vw] mx-auto'>
+                    <h1 className='text-[3vw] font-bold text-center text-strathmore-yellow-light mb-[4vw]'>
+                        Fun, Engaging and Authentic
+                    </h1>
+
+                    <p className='text-[1.36vw] text-center font-normal'>
+                    Lorem ipsum dolor sit amet. In tenetur voluptatem est facere accusantium non itaque aperiam in quia nulla et galisum<br /> quaerat eum labore mollitia sed ipsa quia! Et similique voluptas qui quas dignissimos eos obcaecati dolores eos optio <br /> asperiores. Non illo sunt et laboriosam ipsam et quos itaque cum nisi dolor sed officia sint. Rem consequatur laudantium est <br /> itaque aliquam est ullam excepturi aut internos totam aut voluptatem delectus nam nemo enim!
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
   )
