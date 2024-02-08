cfbd49fae62bac72a42876c8b26ad2557f3a43c6
Martin <martinmmbote@gmail.com>
2024-02-08 11:37:15 +0300

    footer partially done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,25 +4,39 @@ import { strathWhiteLogo } from '../assets';
 const Footer = () => {
   return (
     <div>
-        <div className='mt-[0.7vw] ml-[0.5vw] w-[99vw] h-[40vw] bg-strathmore-blue'>
+        <div className='mt-[0.7vw] ml-[0.5vw] w-[99vw] bg-strathmore-blue'>
             <div>
-                <div className='flex flex-row text-white gap-[3vw] text-[1.1vw]'>
-                    <img src={strathWhiteLogo} className='ml-[6vw] mt-[5.6vw] h-[9vw]'/>
+                <div className='flex flex-row text-white gap-[2.4vw] text-[1.1vw] justify-center'>
+                    <img src={strathWhiteLogo} className='mt-[2vw] mr-[8vw] h-[9vw]'/>
 
-                    <div className='mt-[5.6vw] ml-[6vw]'>
+                    <div className='mt-[2vw]'>
                         <p>Company</p>
-                        <p>About Us</p>
-                        <p>Careers</p>
-                        <p>Educators</p>
-                        <p>Courses</p>
+                        <p className='mt-[0.7vw]'>About Us</p>
+                        <p className='mt-[0.7vw]'>Careers</p>
+                        <p className='mt-[0.7vw]'>Educators</p>
+                        <p className='mt-[0.7vw]'>Courses</p>
                     </div>
                     
-                    <div className='mt-[5.6vw]'>
+                    <div className='mt-[2vw]'>
                         <p>Product</p>
-                        <p>Courses</p>
-                        <p>Pricing</p>
-                        <p>Testimonials</p>
+                        <p className='mt-[0.7vw]'>Courses</p>
+                        <p className='mt-[0.7vw]'>Pricing</p>
+                        <p className='mt-[0.7vw]'>Testimonials</p>
                     </div>
+                </div>
+
+                <div className='text-[0.9vw] flex flex-row gap-[0.5vw] text-white justify-center mt-[2.5vw]'>
+                    <p>
+                        Terms of Service 
+                    </p>
+
+                    <p>
+                        &#x2022;
+                    </p>
+
+                    <p className='mb-[0.5vw]'>
+                        Privacy Policy
+                    </p>
                 </div>
             </div>
         </div>
