42f693f9e225556e71012a3f6c019c7431a2dd7f
Martin <martinmmbote@gmail.com>
2024-02-22 19:45:00 +0300

    video link to github repo added

--- src/components/loggedin/VideoPlayer.jsx
+++ src/components/loggedin/VideoPlayer.jsx
@@ -4,9 +4,19 @@ import ReactPlayer from 'react-player';
 const VideoPlayer = () => {
   return (
     <div className='flex justify-center h-[35vw] border'>
-        <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ'
+        <ReactPlayer url='https://github.com/MartinMbote/elearning-platform/raw/main/src/assets/bikeRide.mp4'
         width='100%'
         height='100%'
+        config={{
+            file: {
+              attributes: {
+                controlsList: 'nodownload' // Optional: To prevent the download button from appearing
+              }
+            },
+            attributes: {
+              controls: true // Optional: To show player controls
+            }
+          }}
         />
     </div>
   )
